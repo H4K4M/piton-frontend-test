7c604de5047a96317a5c321fcdd81ad6c1700347
Hakam Chedo <ha-kam009@hotmail.com>
2024-04-26 15:25:49 +0300

    Contents, TeamMemver

--- components/Contents.js
+++ components/Contents.js
@@ -1,4 +1,5 @@
 import React from "react";
+import TeamMember from "./TeamMember";
 
 export default function Contents() {
   return (
@@ -11,7 +12,8 @@ export default function Contents() {
         project and that this is where splendid emotional relationships between
         the company and people are born.
       </p>
-      <img src="/Image_1.png" className="w-full py-20"></img>
+      <img src="/Image_1.png" className="w-full py-10 sm:py-20"></img>
+      <TeamMember />
     </div>
   );
 }
---
+++ components/TeamMember.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+export default function TeamMember() {
+  return (
+    <div className="">
+      <h1 className="text-base sm:text-4xl font-semibold flex justify-center">
+        Meet the heroes behind the magic
+      </h1>
+      <section className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-3 justify-items-center justify-center gap-y-10 gap-x-14 mt-10 sm:mt-24 mb-3">
+        <div className="w-41 sm:w-82 bg-white shadow-md border-8">
+          <img
+            src="/Avatar_Founder.png"
+            className="sm:h-80 h-40 w-41 sm:w-82 object-cover "
+          />
+          <div className="px-4 py-3 w-41 sm:w-82">
+            <p className="text-sm sm:text-xl font-semibold text-black ">
+              Esther Howard
+            </p>
+            <span className="text-gray-400 mr-3 text-xs sm:text-base">
+              Founder
+            </span>
+          </div>
+        </div>
+
+        <div className="w-41 sm:w-82 bg-white shadow-md border-8">
+          <img
+            src="/Avatar_Dev.png"
+            className="sm:h-80 h-40 w-41 sm:w-82 object-cover "
+          />
+          <div className="px-4 py-3 w-41 sm:w-82">
+            <p className="text-sm sm:text-xl font-semibold text-black ">
+              Cody Fisher
+            </p>
+            <span className="text-gray-400 mr-3 text-xs sm:text-base">
+              Developer
+            </span>
+          </div>
+        </div>
+
+        <div className="w-41 sm:w-82 bg-white shadow-md border-8">
+          <img
+            src="/Avatar_Designer.png"
+            className="sm:h-80 h-40 w-41 sm:w-82 object-cover "
+          />
+          <div className="px-4 py-3 w-41 sm:w-82">
+            <p className="text-sm sm:text-xl font-semibold text-black ">
+              Brooklyn Simmons
+            </p>
+            <span className="text-gray-400 mr-3 text-xs sm:text-base">
+              Designer
+            </span>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
